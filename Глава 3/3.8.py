9663383231b1dc296644e293a7eec6f345785566
Aleksandr <kulaev.aleksandr@yandex.ru>
2024-02-08 17:24:12 +0500

    First 4 part

---
+++ Глава 3/3.8.py
@@ -0,0 +1,17 @@
+# 3.8. Повидать мир
+countries = ['Швейцария', 'Италия', 'США', 'ОАЭ', 'Грузия', 'Франция']
+print(countries)
+print(sorted(countries))
+print(countries)
+print(sorted(countries, reverse=True))
+print(countries)
+countries.reverse()
+print(countries)
+countries.sort()
+print(countries)
+countries.sort(reverse=True)
+print(countries)
+
+#3.9 Определение длины списка - Работает в терминале питона
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+len(cars)
