f6f79ee5385bc6ecb2aa134191b517d07ce22dab
Aleksandr <kulaev.aleksandr@yandex.ru>
2024-02-29 18:11:00 +0500

    Выполнено 6.10

---
+++ Глава 6/6.9.py
@@ -0,0 +1,12 @@
+# 6.9. Любимые места: создайте словарь с именем favorite_places. Придумайте названия трех мест, которые станут ключами словаря,
+# и сохраните для каждого человека от одного до трех любимых мест.
+favorite_places = {
+    'Краснодар': 'Максим',
+    'Минск': {'Саша', 'Максим'},
+    'Екб': {'Кирилл', 'Максим'},
+}
+for place, people in favorite_places.items():
+    for name in people.values():
+    print(f'\nName: {name}')
+    people = people['name']
+    print(f'\tPlace: {place.lower()}')
